53ecab53fef34105c256958ed7c1feaefbce5e4d
Volodarskyi <Volodarskyi@users.noreply.github.com>
2024-12-09 00:44:33 -0800

    feat(security): add "helmet" and "rate limit express"

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
 
 import routes from './routes';
 import { AppError, errorHandler } from './utils/errorHandler';
@@ -14,7 +15,20 @@ dotenv.config();
 app.use(cors({ origin: '*' }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Security
 app.use(helmet());
+app.use(helmet.hsts()); // HTTP Strict Transport Security
+app.use(helmet.noSniff()); // Prevent browsers from sniffing MIME types
+app.use(helmet.xssFilter()); // Prevent XSS attacks
+app.use(helmet.frameguard()); // Prevent clickjacking
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit to 100 requests per window
+});
+
+app.use(limiter);
 
 // API Routes
 app.use('/api', routes);
